80e32d755a272726157b8d202d2635ab733646d8
wzg2572590644 <635811429@qq.com>
2019-04-07 13:49:35 +0800

    Initial commit

---
+++ src/main/java/jenkins/jenkins/com/jenkins/HelloController.java
@@ -0,0 +1,12 @@
+package jenkins.jenkins.com.jenkins;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello jenkins";
+    }
+}
