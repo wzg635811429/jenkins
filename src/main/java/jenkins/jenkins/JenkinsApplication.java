61c5939faa053e260281a4010f04a9e1567f452d
wzg2572590644 <635811429@qq.com>
2019-04-07 14:37:27 +0800

    1

--- src/main/java/jenkins/jenkins/JenkinsApplication.java
+++ src/main/java/jenkins/jenkins/JenkinsApplication.java
@@ -2,12 +2,17 @@ package jenkins.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsApplication {
+public class JenkinsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsApplication.class, args);
     }
-
+    @Override
+    public SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+       return builder.sources(JenkinsApplication.class);
+    }
 }
